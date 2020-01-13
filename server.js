0d807ba14ee029cd72b70f29cbcaf5c0733d2ad4
Danielle B <danibaxx@hotmail.com>
2020-01-12 17:26:12 -0700

    added index.js to start server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
-server.use('/api/projects', ProjectsRouter);
+// server.use('/api/projects', ProjectsRouter);
 
 module.exports = server;
