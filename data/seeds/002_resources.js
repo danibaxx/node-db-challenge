89b45cd0b99afa7f12a05440eb17e37d2fb99f51
Danielle B <danibaxx@hotmail.com>
2020-01-12 19:31:36 -0700

    getting unique constraints error, had to changed 2nd and 3rd resource names to be more unique

--- data/seeds/002_resources.js
+++ data/seeds/002_resources.js
@@ -3,9 +3,9 @@ exports.seed = async (knex) => {
   await knex("resources").insert([
     {resource_name: "CEO", resource_description: "Remodeling", projects_id: 1
     },
-    {resource_name: "Meeting Room", resource_description: "Project planning", projects_id:2
+    {resource_name: "Meeting Room 1", resource_description: "Project planning", projects_id:2
     },
-    {resource_name: "Meeting Room", resource_description: "Finalizing project", projects_id:3
+    {resource_name: "Meeting Room 2", resource_description: "Finalizing project", projects_id:3
     },
   ]);
 };
